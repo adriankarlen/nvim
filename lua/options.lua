721d7fa06e6b5ac7fbbbb4dd20144d22cfd3aa29
Karlén <adrian.karlen@spp.se>
2024-05-03 21:28:45 +0200

    refactor: remove unused code

--- lua/options.lua
+++ lua/options.lua
@@ -60,20 +60,6 @@ for _, provider in ipairs { "node", "perl", "python3", "ruby" } do
   g["loaded_" .. provider .. "_provider"] = 0
 end
 
-vim.diagnostic.config({
-	signs = {
-		text = {
-			[vim.diagnostic.severity.ERROR] = "⏺",
-			[vim.diagnostic.severity.WARN] = "⏺",
-			[vim.diagnostic.severity.INFO] = "⏺",
-			[vim.diagnostic.severity.HINT] = "⏺",
-		},
-	},
-	virtual_text = {
-		prefix = "*",
-	},
-})
-
 -- add binaries installed by mason.nvim to path
 local is_windows = vim.fn.has "win32" ~= 0
 vim.env.PATH = vim.fn.stdpath "data" .. "/mason/bin" .. (is_windows and ";" or ":") .. vim.env.PATH
@@ -106,23 +92,3 @@ autocmd("VimResized", {
   command = "tabdo wincmd =",
 })
 
--- vim.opt.cmdheight = 1
---
--- vim.api.nvim_create_autocmd("CmdlineEnter", {
---   group = vim.api.nvim_create_augroup("cmdheight_1_on_cmdlineenter", { clear = true }),
---   desc = "Don't hide the status line when typing a command",
---   command = ":set cmdheight=1",
--- })
---
--- vim.api.nvim_create_autocmd("CmdlineLeave", {
---   group = vim.api.nvim_create_augroup("cmdheight_0_on_cmdlineleave", { clear = true }),
---   desc = "Hide cmdline when not typing a command",
---   command = ":set cmdheight=0",
--- })
---
--- vim.api.nvim_create_autocmd("BufWritePost", {
---   group = vim.api.nvim_create_augroup("hide_message_after_write", { clear = true }),
---   desc = "Get rid of message after writing a file",
---   pattern = { "*" },
---   command = "redrawstatus",
--- })
