b2844a3e187902eb1e621190451362b54d31f421
Karlén <adrian.karlen@spp.se>
2024-05-03 21:27:32 +0200

    feat: add blinking cursor

--- lua/options.lua
+++ lua/options.lua
@@ -40,6 +40,14 @@ opt.termguicolors = true
 opt.timeoutlen = 400
 opt.undofile = true
 
+-- blinking cursor 
+vim.opt.guicursor = {
+  'n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+  'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+  'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100'
+}
+
+
 -- interval for writing swap file to disk, also used by gitsigns
 opt.updatetime = 250
 
