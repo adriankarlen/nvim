9bc552fa2ca33ce0de3757f5927123171e24a590
Karlén <adrian.karlen@spp.se>
2024-05-06 15:39:48 +0200

    feat: add custom diagnosic icons

--- lua/options.lua
+++ lua/options.lua
@@ -40,13 +40,30 @@ opt.termguicolors = true
 opt.timeoutlen = 400
 opt.undofile = true
 
--- blinking cursor 
+-- blinking cursor
 vim.opt.guicursor = {
-  'n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
-  'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
-  'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100'
+  "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100",
+}
+-- diagnostic configurations
+vim.diagnostic.config {
+  signs = {
+    [vim.diagnostic.severity.ERROR] = "",
+    [vim.diagnostic.severity.WARN] = "",
+    [vim.diagnostic.severity.INFO] = "",
+    [vim.diagnostic.severity.HINT] = "",
+  },
+  virtual_text = {
+    prefix = ">",
+  },
 }
 
+local severities = { "Error", "Warn", "Info", "Hint" }
+for _, type in pairs(severities) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = "", texthl = hl, numhl = hl })
+end
 
 -- interval for writing swap file to disk, also used by gitsigns
 opt.updatetime = 250
@@ -91,4 +108,3 @@ autocmd("VimResized", {
   pattern = "*",
   command = "tabdo wincmd =",
 })
-
