36a8e527a868dee351c585b8cac4cf1784a7340c
Karlén <adrian.karlen@spp.se>
2024-05-16 16:11:27 +0200

    fix: lsp config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,4 +1,15 @@
-local servers = { "eslint", "html", "cssls", "tsserver", "clangd", "lua_ls", "jsonls" }
+local servers = {
+  "tsserver",
+  "denols",
+  "html",
+  "cssls",
+  "svelte",
+  "lua_ls",
+  "emmet_ls",
+  "jsonls",
+  "taplo",
+  "yamlls",
+}
 return {
   "neovim/nvim-lspconfig",
   lazy = false,
@@ -13,39 +24,27 @@ return {
       }
     end
 
-lspconfig.omnisharp.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  cmd = { "C:/Users/ADKA01/AppData/Local/nvim-data/mason/bin/omnisharp.cmd" },
-  -- Enables support for reading code style, naming convention and analyzer
-  -- settings from .editorconfig.
-  enable_editorconfig_support = true,
-  -- If true, MSBuild project system will only load projects for files that
-  -- were opened in the editor. This setting is useful for big C# codebases
-  -- and allows for faster initialization of code navigation features only
-  -- for projects that are relevant to code that is being edited. With this
-  -- setting enabled OmniSharp may load fewer projects and may thus display
-  -- incomplete reference lists for symbols.
-  enable_ms_build_load_projects_on_demand = false,
-  -- Enables support for roslyn analyzers, code fixes and rulesets.
-  enable_roslyn_analyzers = false,
-  -- Specifies whether 'using' directives should be grouped and sorted during
-  -- document formatting.
-  organize_imports_on_format = true,
-  -- Enables support for showing unimported types and unimported extension
-  -- methods in completion lists. When committed, the appropriate using
-  -- directive will be added at the top of the current file. This option can
-  -- have a negative impact on initial completion responsiveness,
-  -- particularly for the first few completion sessions after opening a
-  -- solution.
-  enable_import_completion = true,
-  -- Specifies whether to include preview versions of the .NET SDK when
-  -- determining which version to use for project loading.
-  sdk_include_prereleases = true,
-  -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-  -- true
-  analyze_open_documents_only = false,
-}
+    lspconfig.omnisharp.setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      cmd = { vim.fn.stdpath "data" .. "/mason/bin/omnisharp.cmd" },
+      enable_editorconfig_support = true,
+      enable_roslyn_analysers = true,
+      enable_import_completion = true,
+      organize_imports_on_format = true,
+      enable_decompilation_support = true,
+      -- analyze_open_documents_only = false,
+      filetypes = { "cs", "vb", "csproj", "sln", "slnx", "props", "csx", "targets" },
+    }
+
+    lspconfig.powershell_es.setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      bundle_path = vim.fn.stdpath "data" .. "/mason/packages/powershell-editor-services",
+      init_options = {
+        enableProfileLoading = false,
+      },
+    }
   end,
   keys = {
     { "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", desc = "lsp declaration" },
