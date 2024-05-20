b50149ca47ea5bd8c5da168a50032bb324a4c2fb
Karlén <adrian.karlen@spp.se>
2024-05-20 22:39:06 +0200

    fix: customize markdown icons

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,6 +2,14 @@ return {
   "MeanderingProgrammer/markdown.nvim",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   config = function()
-    require("render-markdown").setup {}
+    require("render-markdown").setup {
+      headings = { "○ ", "○ ", "○ ", "○ ", "○ ", "○ " },
+      checkbox = {
+        -- Character that will replace the [ ] in unchecked checkboxes
+        unchecked = "󰄱 ",
+        -- Character that will replace the [x] in checked checkboxes
+        checked = " ",
+      },
+    }
   end,
 }
