2a8532c5b185ddf14e21a35b58bf0b6f8873bc01
Karlén <adrian.karlen@spp.se>
2024-05-28 11:03:43 +0200

    fix: use vim.treesitter.register instead of filetype_to_parsername

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,19 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      { "windwp/nvim-ts-autotag" },
-    },
-    build = ":TSUpdate",
-    config = function()
-      local config = require "nvim-treesitter.configs"
-      config.setup {
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-        autopairs = { enable = true },
-        autotag = { enable = true },
-      }
-
-      local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
-      ft_to_parser.mdx = "markdown"
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    { "windwp/nvim-ts-autotag" },
   },
+  build = ":TSUpdate",
+  config = function()
+    local config = require "nvim-treesitter.configs"
+    config.setup {
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      autopairs = { enable = true },
+      autotag = { enable = true },
+    }
+
+    vim.treesitter.language.register("markdown", "mdx")
+  end,
 }
