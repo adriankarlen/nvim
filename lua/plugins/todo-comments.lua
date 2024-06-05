a47482fb14e570fe97eba86ba918e6d2b990e96d
Karlén <adrian.karlen@spp.se>
2024-06-05 10:45:06 +0200

    fix: todo comments keybind

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,11 +1,9 @@
 return {
-  -- Show all todo comments in solution
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {},
-  -- HACK: Add setup function
   event = "BufRead",
   keys = {
-    { "<leader>fc", "<cmd>TodoTelescope<cr>", "telescope - todo comments" },
+    { "<leader>fc", "<cmd>TodoTelescope<cr>", desc = "telescope - todo comments" },
   },
 }
