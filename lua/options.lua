717b361a5816a4cbab0618e664fb415f98aeb860
adriankarlen <adrian.karlen@spp.se>
2024-06-11 12:07:33 +0200

    fix: dont use blinking cursor

--- lua/options.lua
+++ lua/options.lua
@@ -41,12 +41,6 @@ opt.termguicolors = true
 opt.timeoutlen = 400
 opt.undofile = true
 
--- blinking cursor
-opt.guicursor = {
-  "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
-  "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
-  "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100",
-}
 -- diagnostic configurations
 vim.diagnostic.config {
   signs = {
