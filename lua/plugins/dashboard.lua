2cb3c08442a89cadf2a508d5fe971a8c6c690ebe
adriankarlen <adrian.karlen@spp.se>
2024-06-20 13:38:01 +0200

    feat!: migrate to dashboard from alpha

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -20,38 +20,31 @@ local logo = {
 }
 
 return {
-  "goolord/alpha-nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-    "nvim-lua/plenary.nvim",
-  },
-
+  "nvimdev/dashboard-nvim",
+  event = "VimEnter",
   config = function()
-    local alpha_th = require "alpha.themes.theta"
-    local alpha_db = require "alpha.themes.dashboard"
-    alpha_th.header.val = logo
-    alpha_th.header.opts.hl = "AlphaHeader"
-    alpha_th.buttons.val = {
-      {
-        type = "text",
-        val = "╭" .. string.rep("─", 48) .. "╮",
-        opts = { hl = "FloatBorder", position = "center" },
-      },
-      alpha_db.button("i", "󰈔    new file", "<cmd>ene <BAR> startinsert<CR>"),
-      alpha_db.button("o", "󱋡    old files", "<cmd>Telescope oldfiles<CR>"),
-      alpha_db.button("f", "󰥨    find file", "<cmd>Telescope find_files<CR>"),
-      alpha_db.button("w", "󰱼    find text", "<cmd>Telescope live_grep<CR>"),
-      alpha_db.button("g", "    browse git", "<cmd>LazyGit<CR>"),
-      alpha_db.button("l", "󰒲    lazy", "<cmd>Lazy<CR>"),
-      alpha_db.button("m", "󱌣    mason", "<cmd>Mason<CR>"),
-      alpha_db.button("p", "󰓅    profile", "<cmd>Lazy profile<CR>"),
-      alpha_db.button("q", "󰭿    quit", "<cmd>qa<CR>"),
-      {
-        type = "text",
-        val = "╰" .. string.rep("─", 48) .. "╯",
-        opts = { hl = "FloatBorder", position = "center" },
+    require("dashboard").setup {
+      theme = "hyper",
+      shortcut_type = "number",
+      config = {
+        header = logo,
+        packages = { enable = false },
+        project = { enable = false },
+        shortcut = {
+          { desc = "󰥨 find files", group = "DiagnosticWarn", key = "f", action = "Telescope find_files" },
+          { desc = " browse git", group = "DiagnosticWarn", key = "g", action = "LazyGit" },
+          { desc = "󰒲 lazy", group = "DiagnosticWarn", key = "l", action = "Lazy" },
+          { desc = "󱌣 mason", group = "DiagnosticWarn", key = "m", action = "Mason" },
+          { desc = "󰭿 quit", group = "DiagnosticWarn", key = "q", action = "qa" },
+        },
+        footer = {
+          [[                                          ]],
+          [[                                          ]],
+          [[                                          ]],
+          [[let's introduce some bugs and fuck shit up]],
+        },
       },
     }
-    require("alpha").setup(alpha_th.config)
   end,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
 }
