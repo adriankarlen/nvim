bba38910443524d5474751548b370bfbfbaa7260
adriankarlen <adrian.karlen@spp.se>
2024-06-27 07:45:04 +0200

    fix: add spellwarn toggle

--- lua/plugins/spell_warn.lua
+++ lua/plugins/spell_warn.lua
@@ -1,5 +1,19 @@
 return {
-    "ravibrock/spellwarn.nvim",
-    event = "VeryLazy",
-    config = true,
+  "ravibrock/spellwarn.nvim",
+  event = "BufEnter",
+  config = true,
+
+  keys = {
+    {
+      "<leader>sw",
+      function()
+        require("spellwarn").toggle()
+      end,
+      {
+        noremap = true,
+        silent = true,
+        description = "spellwarn - toggle",
+      },
+    },
+  },
 }
