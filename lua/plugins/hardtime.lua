cf952af037a6f79eb686f16312ebafb1c5b99a74
adriankarlen <adrian.karlen@spp.se>
2024-07-21 01:16:07 +0200

    feat: add hardtime

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,8 @@
+return {
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  opts = {},
+  keys = {
+    { "<leader>th", "<cmd>Hardtime toggle<cr>", desc = "hardtime - toggle" },
+  },
+}
