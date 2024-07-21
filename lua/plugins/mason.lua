a63978bcf430da987a4676ef313f0a1aca7c25cf
adriankarlen <adrian.karlen@spp.se>
2024-07-21 11:13:20 +0200

    fix: mason styling

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -15,21 +15,11 @@ return {
       max_concurrent_installers = 10,
       ui = {
         icons = {
-          package_pending = " ",
-          package_installed = "󰄳 ",
-          package_uninstalled = "󰚌 ",
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = ""
         },
-
-        keymaps = {
-          toggle_server_expand = "<CR>",
-          install_server = "i",
-          update_server = "u",
-          check_server_version = "c",
-          update_all_servers = "U",
-          check_outdated_servers = "C",
-          uninstall_server = "X",
-          cancel_installation = "<C-c>",
-        },
+        border = "single",
       },
     }
 
