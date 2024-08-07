1ff3663eb306e9fead6b96f7bfbaa829f8400c9b
adriankarlen <adrian.karlen@spp.se>
2024-08-07 11:59:19 +0200

    fix: add buffers to cmp

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -101,6 +101,7 @@ return {
           { name = "buffer", group_index = 2 },
           { name = "nvim_lua", group_index = 2 },
           { name = "path", group_index = 2 },
+          { name = "buffer", group_index = 2 },
           { name = "cmdline", group_index = 2 },
           {
             name = "html-css",
