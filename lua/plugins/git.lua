926ac624aa711907e473cf3eaa74c870ab9b84cf
adriankarlen <adrian.karlen@spp.se>
2024-09-03 11:04:10 +0200

    refactor: keep all git plugins in same file

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,8 @@
 return {
   {
+    "tpope/vim-fugitive",
+  },
+  {
     event = "BufReadPre",
     "lewis6991/gitsigns.nvim",
     config = function()
@@ -13,6 +16,26 @@ return {
       { "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<cr>", desc = "undo stage hunk" },
       { "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "blame line" },
       { "<leader>gd", "<cmd>Gitsigns toggle_deleted<cr>", desc = "toggle deleted" },
+    },
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      vim.g.lazygit_floating_window_border_chars = { "┌", "─", "┐", "│", "┘", "─", "└", "│" }
+    end,
+    keys = {
+      { "<leader>gl", "<cmd>LazyGit<cr>", desc = "lazygit" },
     },
   },
 }
