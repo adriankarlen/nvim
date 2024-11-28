6b445f377dc5b625db1fc8363b2a32e2034248a2
adriankarlen <adrian.karlen@spp.se>
2024-11-28 06:39:59 +0100

    fix: ftplugin issue

--- ftplugin/codecompanion.lua
+++
@@ -1,1 +0,0 @@
-vim.opt.rnu = false
--- ftplugin/jsonc.lua
+++ ftplugin/jsonc.lua
@@ -1,5 +1,5 @@
 -- When creating a new line with o, make sure there is a trailing comma on the
 -- current line
 vim.keymap.set("n", "o", function()
-  return require("j.json").add_trailing_comma_if_needed()
+  return require("utils.json").add_trailing_comma_if_needed()
 end, { buffer = true, expr = true })
