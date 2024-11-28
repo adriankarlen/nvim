30c631767b9ef6622d97820e949428f4b2dd6b3e
adriankarlen <adrian.karlen@spp.se>
2024-11-28 06:40:32 +0100

    fix: go back to codecompantion for ai chat

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,97 +1,50 @@
-local M = {}
-
-function M.pick(kind)
-  return function()
-    local actions = require "CopilotChat.actions"
-    local items = actions[kind .. "_actions"]()
-    if not items then
-      vim.notify("No " .. kind .. " found on the current line", 3)
-      return
-    end
-    require("CopilotChat.actions").pick(items)
-  end
-end
-
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        help = true,
+  "olimorris/codecompanion.nvim",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    {
+      "zbirenbaum/copilot.lua",
+      cmd = "Copilot",
+      build = ":Copilot auth",
+      opts = {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
       },
     },
   },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    cmd = "CopilotChat",
-    opts = function()
-      local user = vim.env.USER or ""
-      user = user:sub(1, 1):upper() .. user:sub(2)
-      return {
-        auto_insert_mode = true,
-        show_help = true,
-        question_header = "  " .. user .. " ",
-        answer_header = "  copilot ",
-        window = {
-          width = 0.4,
+  opts = {
+    strategies = {
+      inline = {
+        adapter = "copilot",
+      },
+      chat = {
+        adapter = "copilot",
+        roles = {
+          llm = " Copilot",
+          user = " Me",
         },
-        selection = function(source)
-          local select = require "CopilotChat.select"
-          return select.visual(source) or select.buffer(source)
-        end,
-      }
-    end,
-    keys = {
-      { "<c-s>", "<CR>", ft = "copilot-chat", desc = "submit", remap = true },
-      {
-        "<leader>aa",
-        function()
-          return require("CopilotChat").toggle()
-        end,
-        desc = "toggle chat",
-        mode = { "n", "v" },
       },
-      {
-        "<leader>ax",
-        function()
-          return require("CopilotChat").reset()
-        end,
-        desc = "clear chat",
-        mode = { "n", "v" },
+      display = {
+        chat = {
+          render_headers = false,
+        },
       },
-      {
-        "<leader>aq",
-        function()
-          local input = vim.fn.input "quick chat: "
-          if input ~= "" then
-            require("CopilotChat").ask(input)
-          end
-        end,
-        desc = "quick chat",
-        mode = { "n", "v" },
-      },
-      -- show help
-      { "<leader>ad", M.pick "help", desc = "diagnostic help", mode = { "n", "v" } },
-      -- show prompts actions
-      { "<leader>ap", M.pick "prompt", desc = "prompt actions", mode = { "n", "v" } },
     },
-    config = function(_, opts)
-      local chat = require "CopilotChat"
-
-      vim.api.nvim_create_autocmd("BufEnter", {
-        pattern = "copilot-chat",
-        callback = function()
+  },
+  keys = {
+    { "<leader>aa", "<cmd>CodeCompanionActions<cr>", mode = { "n", "x" }, desc = "actions" },
+    { "<leader>at", "<cmd>CodeCompanionToggle<cr>", mode = { "n", "x" }, desc = "toggle" },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function(opts)
+        if vim.bo[opts.buf].filetype == "codecompanion" then
           vim.opt_local.relativenumber = false
           vim.opt_local.number = false
-        end,
-      })
-      chat.setup(opts)
-    end,
-  },
+        end
+      end,
+    })
+  end,
 }
