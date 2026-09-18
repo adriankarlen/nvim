3336098613c2c89b6df375c097e40f2478f9ae48
Adrian Karlen <113891532+adriankarlen@users.noreply.github.com>
2026-09-18 15:10:51 +0200

    fix: stop explicitly enabling filetype plugins and syntax support

--- plugin/10_options.lua
+++ plugin/10_options.lua
@@ -10,12 +10,6 @@ vim.o.timeoutlen = 400 -- Faster key sequence timeout
 
 vim.o.shada = "'100,<50,s10,:1000,/100,@100,h" -- Limit ShaDa file (for startup)
 
--- Enable all filetype plugins and syntax (if not enabled, for better startup)
-vim.cmd "filetype plugin indent on"
-if vim.fn.exists "syntax_on" ~= 1 then
-  vim.cmd "syntax enable"
-end
-
 -- Disable some default providers
 for _, provider in ipairs { "node", "perl", "python3", "ruby" } do
   vim.g["loaded_" .. provider .. "_provider"] = 0
